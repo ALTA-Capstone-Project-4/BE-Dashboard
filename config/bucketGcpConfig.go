404ad1a72accd02ec7aafbe43bf46e386838c4ef
muhdwiar <muhdwi45@gmail.com>
2022-09-28 23:31:58 +0700

    add upload image

---
+++ config/bucketGcpConfig.go
@@ -0,0 +1,61 @@
+package config
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"mime/multipart"
+	"os"
+	"time"
+
+	"cloud.google.com/go/storage"
+)
+
+const (
+	projectID  = "PROJECT_ID"  // FILL IN WITH YOURS
+	bucketName = "BUCKET_NAME" // FILL IN WITH YOURS
+)
+
+type ClientUploader struct {
+	cl         *storage.Client
+	projectID  string
+	bucketName string
+	uploadPath string
+}
+
+var Uploader *ClientUploader
+
+func init() {
+	os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", "") // FILL IN WITH YOUR FILE PATH
+	client, err := storage.NewClient(context.Background())
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	Uploader = &ClientUploader{
+		cl:         client,
+		bucketName: bucketName,
+		projectID:  projectID,
+		uploadPath: "ktp-files/",
+	}
+
+}
+
+func (c *ClientUploader) UploadFile(file multipart.File, object string) error {
+	ctx := context.Background()
+
+	ctx, cancel := context.WithTimeout(ctx, time.Second*50)
+	defer cancel()
+
+	// Upload an object with storage.Writer.
+	wc := c.cl.Bucket(c.bucketName).Object(c.uploadPath + object).NewWriter(ctx)
+	if _, err := io.Copy(wc, file); err != nil {
+		return fmt.Errorf("io.Copy: %v", err)
+	}
+	if err := wc.Close(); err != nil {
+		return fmt.Errorf("Writer.Close: %v", err)
+	}
+
+	return nil
+}
