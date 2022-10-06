b513245ece9a5d5ee1f096fe2cc3cb96f8404371
muhdwiar <muhdwi45@gmail.com>
2022-10-07 06:43:58 +0700

    make cron for update status lahan

--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"time"
 	"warehouse/config"
 	"warehouse/factory"
+	modelCheckout "warehouse/features/checkout/data"
+	modelLahan "warehouse/features/lahan/data"
 	"warehouse/migration"
 	"warehouse/utils/database/mysql"
 
+	"github.com/go-co-op/gocron"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -19,6 +23,46 @@ func main() {
 
 	e := echo.New()
 	e.Pre(middleware.RemoveTrailingSlash())
+
+	gmt, _ := time.LoadLocation("Asia/Jakarta")
+	s := gocron.NewScheduler(gmt)
+	s.Every(1).Day().At("00:00").Do(
+		func() {
+			// Select checkout expired today
+			var dataCheckout []modelCheckout.Checkout
+
+			layout_date := "2006-01-02"
+			tx_select := db.Where("akhir_sewa = ?", time.Now().Format(layout_date)+" 07:00:00.000").Find(&dataCheckout)
+
+			if tx_select.Error != nil {
+				fmt.Println("gagal mencari checkout yang expired")
+			} else {
+				fmt.Println("berjasil mencari checkout yang expired")
+			}
+
+			// Update status lahan
+			for _, v := range dataCheckout {
+				var data modelLahan.Lahan
+				tx_selectLahan := db.Where("id = ?", v.LahanID).Preload("Gudang").Find(&data)
+
+				if tx_selectLahan.Error != nil {
+					fmt.Println("gagal mencari lahan")
+				} else {
+					fmt.Println("berhasil mencari lahan")
+				}
+
+				data.Status = "tidak disewa"
+				tx_update := db.Model(&modelLahan.Lahan{}).Where("id = ?", data.ID).Updates(&data)
+
+				if tx_update.Error != nil {
+					fmt.Println("gagal update status lahan")
+				} else {
+					fmt.Println("update status lahan berjalan")
+				}
+			}
+		},
+	)
+	s.StartAsync()
 
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
